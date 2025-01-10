8bd4b17a563c837f449c5e571599da3a590ce6ed
psdekate <piyushdekate29@gmail.com>
2025-01-10 23:24:02 +0530

    handling count using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import "./App.css";
-import { Header } from "./components/Header/Header";
-import { Main } from "./components/Main/Main";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function handleAddition() {
+    setCount(count + 1);
+  }
+
+  function handleRemove() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+
   return (
     <>
       <div className="container">
-        <Header />
-        <Main />
+        <div className="counter-wrapper">
+          <button className="action" onClick={handleAddition}>
+            add
+          </button>
+          <div>
+            <p className="count">{count}</p>
+          </div>
+          <button className="action" onClick={handleRemove}>
+            remove
+          </button>
+        </div>
       </div>
     </>
   );
