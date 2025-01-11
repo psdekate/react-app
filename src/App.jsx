a7b2f9d51e7507cec4491f77141961a1b464d96e
psdekate <piyushdekate29@gmail.com>
2025-01-11 19:29:59 +0530

    form-events

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,67 @@
 import "./App.css";
-import { votes } from "./data3";
 import { useState } from "react";
 
-let initialData = [
-  {
-    name: "Jason",
-    description: "We will make justice",
-    votes: 0,
-  },
-  {
-    name: "Carter",
-    description: "We shall and we will win",
-    votes: 0,
-  },
-  {
-    name: "Smith",
-    description: "The truth is with us",
-    votes: 0,
-  },
-];
-
 function App() {
-  const [vote, setVote] = useState(initialData);
-  const [votedIndex, setVotedIndex] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [submittedData, setSubmittedData] = useState(null);
 
-  function handleVoteCount(index) {
-    // const updatedData = [...vote];
-    // updatedData[index].votes += 1;
-    // setVote(updatedData);
-    // setVotedIndex(index);
-    setVote((prevData) => {
-      const updatedData = [...prevData];
-      updatedData[index] = { ...updatedData[index], votes: updatedData[index].votes + 1 };
-      return updatedData;
-    });
-    setVotedIndex(index);
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setSubmittedData({ name, email, message });
+
+    setName = "";
+    setEmail = "";
+    setMessage = "";
   }
-
-  const maxVotes = Math.max(...vote.map((item) => item.votes));
 
   return (
     <>
       <div className="container">
-        <div className="options-grid">
-          {vote.map((item, index) => (
-            <div
-              key={item.name}
-              className={item.votes > 0 && item.votes === maxVotes ? "option max" : "option"}
-            >
-              <h2>{item.name}</h2>
-              <p>"{item.description}"</p>
-              <p>Votes: {item.votes}</p>
-              <button onClick={() => handleVoteCount(index)} disabled={votedIndex === index}>
-                {votedIndex === index ? "Voted" : "Vote"}
-              </button>
-            </div>
-          ))}
+        <div className="form-wrapper">
+          <div className="title-and-desc">
+            <h2>Submit a form</h2>
+            <p>Your queries will be reviewed later</p>
+            <form action="" className="form" onSubmit={handleSubmit}>
+              <div className="form-section">
+                <label htmlFor="formField">Name</label>
+                <input
+                  type="text"
+                  id="formField"
+                  placeholder="Enter your name here"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="form-section">
+                <label htmlFor="formField">Email</label>
+                <input
+                  type="email"
+                  id="formField"
+                  placeholder="Enter your email here"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="form-section">
+                <label htmlFor="formField">Message</label>
+                <textarea
+                  name=""
+                  id="formField"
+                  placeholder="Enter your message here"
+                  onChange={(e) => setMessage(e.target.value)}
+                ></textarea>
+              </div>
+              <button>Submit</button>
+              {submittedData && (
+                <div>
+                  <h2>Name you entered: {submittedData.name}</h2>
+                  <p>Email you entered: {submittedData.email}</p>
+                  <p>Message you entered: {submittedData.message}</p>
+                </div>
+              )}
+            </form>
+          </div>
         </div>
       </div>
     </>
